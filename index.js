5a2ecf12600f17397ebba8cb787be37ded704d31
Colin Grahm <developer@colingrahm.de>
2023-08-30 12:33:51 +0200

    Setup window style

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+'use strict';
+
 const { app, BrowserWindow } = require('electron')
+const electronReload = require('electron-reload')
 
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      autoHideMenuBar: true,
+      icon: __dirname + '/assets/icon.png'
     })
   
     win.loadFile('index.html')
