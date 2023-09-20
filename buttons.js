27ed7e7b6b6a6f42baeff146ab15492adf8875f6
Colin Grahm <developer@colingrahm.de>
2023-09-20 15:50:20 +0200

    final final final

--- buttons.js
+++ buttons.js
@@ -2,8 +2,8 @@ const execSh = require("exec-sh");
 
 var pyPath = "C:/Users/Colin/AppData/Local/Programs/Python/Python311/python.exe"; // NOTE Enter your PATH to Python interpreter
 var route_a = pyPath + ' ' + 'Harald.py'; // NOTE Enter PATH to route file
-var route_b = pyPath + ' ' + 'Brigitte.py'; // NOTE Enter PATH to route file
-var route_c = pyPath + ' ' + 'Otto.py'; // NOTE Enter PATH to route file
+var route_b = pyPath + ' ' + 'Harald.py'; // NOTE Enter PATH to route file
+var route_c = pyPath + ' ' + 'Harald.py'; // NOTE Enter PATH to route file
 const buttonA = document.getElementById("route-a-btn"); 
 const buttonB = document.getElementById("route-b-btn");
 const buttonC = document.getElementById("route-c-btn");
